a540ab507cd79e1222e93a76d1395b61ce35831a
Alan Forneron <alan.forneron@naranja.com>
2023-04-08 11:10:05 -0300

    adds a generic onbnoarding screen to be reused

--- Bankey/Bankey/Onboarding/OnboardingContainerViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -19,9 +19,9 @@ class OnboardingContainerViewController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
+        let page1 = OnboardingViewController(heroImage: "delorean", titleText: "Soy el titulo para delorean")
+        let page2 = OnboardingViewController(heroImage: "thumbs", titleText: "Soy el titulo para thumbs")
+        let page3 = OnboardingViewController(heroImage: "world", titleText: "Soy el titulo para world")
         
         pages.append(page1)
         pages.append(page2)
@@ -89,27 +89,5 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
 
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return pages.firstIndex(of: self.currentVC) ?? 0
-    }
-}
-
-// MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
     }
 }
