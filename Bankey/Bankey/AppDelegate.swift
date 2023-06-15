a35346fbd75dabacc84af5e2b161c509489bd949
Alan Forneron <alan.forneron@naranja.com>
2023-06-15 15:15:27 -0300

    refactor appdelegate

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -11,52 +11,43 @@ let appColor: UIColor = .systemTeal
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
     let loginViewController = LoginViewController()
-    let onboardingCVC = OnboardingContainerViewController()
+    let onboardingViewController = OnboardingContainerViewController()
     let mainViewController = MainViewController()
-    let accountViewController = AccountSummaryViewController()
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
+
         loginViewController.delegate = self
-        onboardingCVC.delegate = self
+        onboardingViewController.delegate = self
+
+        displayLogin()
+        return true
+    }
+
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+
+    private func displayNextScreen() {
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingViewController)
+        }
+    }
+
+    private func prepMainView() {
         mainViewController.setStatusBar()
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().backgroundColor = appColor
-        window?.rootViewController = mainViewController
-        
-        return true
     }
-    
-}
-
-extension AppDelegate: OnboardingContainerViewControllerDelegate {
-    func didFinishOnboarding() {
-        LocalState.hasOnboarded = true
-        setRootViewController(mainViewController)
-    }
-}
-
-extension AppDelegate: LoginViewControllerDelegate {
-    func didLogin() {
-        if LocalState.hasOnboarded {
-            setRootViewController(mainViewController)
-        } else {
-            setRootViewController(onboardingCVC)
-        }
-    }
-}
-
-extension AppDelegate: LogoutViewControllerDelegate {
-    func didLogout() {
-        setRootViewController(loginViewController)
-    }
-    
 }
 
 extension AppDelegate {
@@ -66,11 +57,33 @@ extension AppDelegate {
             self.window?.makeKeyAndVisible()
             return
         }
-        
+
         window.rootViewController = vc
         window.makeKeyAndVisible()
         UIView.transition(with: window,
                           duration: 0.3,
-                          animations: nil)
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
+    }
+}
+
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        displayNextScreen()
+    }
+}
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        LocalState.hasOnboarded = true
+        prepMainView()
+        setRootViewController(mainViewController)
+    }
+}
+
+extension AppDelegate: LogoutViewControllerDelegate {
+    func didLogout() {
+        setRootViewController(loginViewController)
     }
 }
